3e47af9f2adf6579f7da742fcbf5980e946074af
bkrsjak <bkrsjak@newpush.com>
2021-10-22 14:24:48 +0200

    14:24

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+import axios from "axios";
+import VueAxios from 'vue-axios';
+Vue.use(VueAxios, axios);
+
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 Vue.config.productionTip = false;
 
